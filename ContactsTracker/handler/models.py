c39a852b2f0d52a360920c3b61dfd256f5ea1c0e
nagasai-iitr <madamshettinagasai@gmail.com>
2024-04-24 01:03:48 +0530

    create model for contact table

---
+++ ContactsTracker/handler/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+class Contact(models.Model):
+    class ContactType(models.TextChoices):
+        primary = 'primary'
+        secondary = 'secondary'
+        
+    phoneNumber = models.CharField(max_length=200,null=True, blank=True)
+    email = models.CharField(max_length=200, null=True, blank=True)
+    linkedId = models.IntegerField(null=True, blank=True)
+    linkPrecedence = models.CharField(choices=ContactType, default=ContactType.primary)
+    createdAt = models.DateTimeField(auto_now_add=True,)
+    updatedAt = models.DateTimeField(auto_now=True)
+    deletedAt = models.DateTimeField(null=True, blank=True)
+    
+    class Meta:
+        db_table = 'Contact'
